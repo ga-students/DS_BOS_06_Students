1248dd9581d5a3d59a9be5b8acaa722777df2207
yasan61 <adam.yasan@gmail.com>
2015-07-07 13:13:41 -0400

    Revert "Question1-assig1"
    This reverts commit f5bffc76a9ce326e0ce1278b52449f8d1a50085f.

--- Adam/Question1-A1.sql
+++
@@ -1,5 +0,0 @@
-Select nameLast, nameFirst, (H/AB) as BA, RBI, HR
-From master, battingpost, awardsplayers
-where (awardsplayers.playerID = battingpost.playerID and awardID = 'Triple Crown')
-and battingpost.yearID = awardsplayers.yearID
-order by BA, RBI, HR;
--- Adam/Question3 -A1.sql
+++
@@ -1,6 +0,0 @@
-Select teamID, count(allstarfull.teamID) as total 
-FROM allstarfull
-JOIN awardsplayers
-ON awardsplayers.playerID = allstarfull.playerID
-where (awardID = 'Most Valuable Player' or awardID = 'Triple Crown')
-Group by teamID;
